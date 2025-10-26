e38d31cff5626123fc03a4049fa6c7cc4138e9b1
Irthir <romain.schlotter@gmail.com>
2025-10-26 21:12:30 +0100

    Github Actions

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Remplace "tonNomUtilisateur" et "shadowverse-tracker" par ton nom GitHub et le nom du repo
 export default defineConfig({
-  base: '/shadowverse-tracker/', 
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: '/shadowverse-tracker/',
+  plugins: [react({ babel: { plugins: [['babel-plugin-react-compiler']] } })],
 });
