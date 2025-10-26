a717fd2300d7d95a5178998d34eb9ff3e4841ea3
Irthir <romain.schlotter@gmail.com>
2025-10-26 21:40:28 +0100

    Revision vite.config

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // <<< changer de '/shadowverse-tracker/' à './'
-  plugins: [react({ babel: { plugins: [['babel-plugin-react-compiler']] } })],
-});
+  plugins: [react()],
+  base: '/shadowverse-tracker/',
+})
